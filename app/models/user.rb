c041ec86ea6525e8c3e22c95e8a83faaf1202eac
Arata-Ikeda <ikeda_ar@tokium.jp>
2025-06-06 19:30:16 +0900

    ユーザー登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
+  has_one_attached :icon
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: 15 }
+  validates :icon, presence: true
+
   # OmniAuthからの情報を使ってユーザーを検索または作成するクラスメソッド
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(google_id: auth.uid).first_or_initialize.tap do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
+      user.google_id = auth.uid
+      user.icon_url = auth.info.image
+      user.save!
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   post 'logout', to: 'sessions#destroy', as: 'logout'
- 
+
+  get 'register', to: 'registrations#new', as: :new_user_registration
+  post 'register', to: 'registrations#create', as: :user_registration
+
   get "up" => "rails/health#show", as: :rails_health_check
   
 end
